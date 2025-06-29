b86f32045dbd24c2cde9067fe9240ce7752275d2
Lucas van Dijk <info@lucasvandijk.nl>
2025-06-29 18:32:55 -0400

    feat: Add RoPE implementation

---
+++ src/flamino/rope.py
@@ -0,0 +1,82 @@
+"""
+Flax NNX-based implementation of the RoPE (Rotary Positional Embeddings) technique.
+"""
+
+import jax
+import jax.numpy as jnp
+from flax import nnx
+
+
+def apply_rope(x: jax.Array, sin: jax.Array, cos: jax.Array) -> jax.Array:
+    assert x.ndim == 2, "No batch dimension allowed for x, expected dimensions (seq_len, embedding_dim)"
+    seq_len = x.shape[0]
+
+    sin = sin[:seq_len, :]
+    cos = cos[:seq_len, :]
+
+    return (x * cos) + (rotate_half(x) * sin)
+
+
+def rotate_half(x: jax.Array) -> jax.Array:
+    x1, x2 = jnp.split(x, 2, axis=-1)
+    return jnp.concatenate([-x2, x1], axis=-1)
+
+
+class Frequencies(nnx.Variable[jax.Array]):
+    pass
+
+
+class SinCosTable(nnx.Variable[jax.Array]):
+    pass
+
+
+# No batching of frequency vector and sin/cos tables
+state_axes = nnx.StateAxes({
+    Frequencies: None,
+    SinCosTable: None,
+})
+
+
+class RoPE(nnx.Module):
+    """
+    Implementation of the RoPE (Rotary Positional Encodings) technique.
+
+    Computes caches the sin and cos matrices on-the-fly, depending on the given sequence length.
+    """
+
+    def __init__(self, d_embed: int, theta: float = 10000.0, *, rngs: nnx.Rngs):
+        if d_embed % 2 != 0:
+            raise ValueError(f"`d_embed` must be even, got {d_embed}.")
+
+        self.d_embed: int = d_embed
+        self.theta: float = theta
+
+        self._freqs: Frequencies = Frequencies(1.0 / (self.theta ** (jnp.arange(0, self.d_embed, 2) / self.d_embed)))
+        self._cached_sin: SinCosTable = SinCosTable(jnp.zeros(0))
+        self._cached_cos: SinCosTable = SinCosTable(jnp.zeros(0))
+
+        _, _ = self._update_cache(1024)
+
+    def _update_cache(self, seq_len: int) -> tuple[jax.Array, jax.Array]:
+        seq_len_cached = self._cached_sin.value.shape[0] if self._cached_sin.value is not None else 0
+
+        if (
+            self._cached_sin.value is None
+            or self._cached_cos.value is None
+            or seq_len > seq_len_cached
+        ):
+            freqs = self._freqs.value
+            freqs = jnp.concatenate([freqs, freqs], axis=-1)
+            positions = jnp.arange(seq_len)
+            angles = jnp.outer(positions, freqs)
+
+            self._cached_sin = SinCosTable(jnp.sin(angles))
+            self._cached_cos = SinCosTable(jnp.cos(angles))
+
+        return self._cached_sin.value, self._cached_cos.value
+
+    @nnx.vmap(in_axes=(state_axes, 0), out_axes=0)
+    def __call__(self, x: jax.Array):
+        sin_table, cos_table = self._update_cache(x.shape[0])
+
+        return apply_rope(x, sin_table, cos_table)
---
+++ tests/test_rope.py
@@ -0,0 +1,52 @@
+import jax
+import jax.numpy as jnp
+from flax import nnx
+
+from flamino.vocab import Alphabet
+from flamino.rope import RoPE
+
+
+def test_relative_encoding():
+    vocab = Alphabet.amino_acids()
+    d_embed = 128
+    rngs = nnx.Rngs(42)
+    key = jax.random.key(42)
+    
+    mock_embeddings = {
+        token: jax.random.normal(key, (d_embed,))
+        for token in vocab.tokens
+    }
+    
+    seqs = ["ACAC", "FFGG"]
+    seqs = jnp.concatenate([
+        jnp.array([mock_embeddings[token] for token in seq])[None, :]  # Add a new axis for batch dimension
+        for seq in seqs
+    ])
+    
+    rope = RoPE(d_embed, rngs=rngs)
+    rope_encoding = rope(seqs)
+    
+    assert rope_encoding.shape == (2, seqs.shape[1], d_embed)
+    
+    ac1 = rope_encoding[0, :2, :]
+    ac2 = rope_encoding[0, 2:, :]
+    
+    # We expect the embeddings to be different, now with position encoded
+    assert jnp.sum(ac1[1] == seqs[0, 1, :]) != jnp.array(d_embed)
+    
+    # Inner product between two identical tokens should be the same regardless of 
+    # their absolute position
+    inner1 = jnp.inner(ac1[0], ac1[1])
+    inner2 = jnp.inner(ac2[0], ac2[1])
+    
+    assert jnp.allclose(inner1, inner2)
+    
+    f1 = rope_encoding[1, 0, :]
+    f2 = rope_encoding[1, 1, :]
+    g1 = rope_encoding[1, 2, :]
+    g2 = rope_encoding[1, 3, :]
+    
+    inner1 = jnp.inner(f1, g1)
+    inner2 = jnp.inner(f2, g2)
+    
+    assert jnp.allclose(inner1, inner2)
