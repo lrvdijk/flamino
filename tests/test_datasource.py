05b71f5967077c8f4396ebf4f51c346d3a5bf361
Lucas van Dijk <info@lucasvandijk.nl>
2025-08-08 17:24:54 -0400

    fix: Fix datasource tests

--- tests/test_datasource.py
+++ tests/test_datasource.py
@@ -2,15 +2,12 @@ from flamino.data.datasource import FastaDataSource
 
 
 def test_fasta_data_source():
-    fasta_files = [
-        "tests/test_data/test_fasta1.fa",
-        "tests/test_data/test_fasta2.fa"
-    ]
-    
+    fasta_files = ["tests/test_data/test_fasta1.fa.gz", "tests/test_data/test_fasta2.fa.gz"]
+
     fasta_source = FastaDataSource(fasta_files)
     assert len(fasta_source) == 5
     assert fasta_source.num_seq_per_file == [3, 2]
-    
+
     assert fasta_source[0] == "AAAGGTT"
     assert fasta_source[1] == "CCCGGTT"
     assert fasta_source[4] == "CCCCCCCCC"
